d4ab2dc8f34b8732347017892fe0eee92d8166d3
Ben Jeffrey <mail@benjeffrey.net>
2017-11-15 23:58:40 +0000

    Name backup file according to date.

--- backup.js
+++ backup.js
@@ -1,9 +1,11 @@
 const AWS = require("aws-sdk");
 const UploadStream = require("s3-stream-upload");
 const got = require('got');
+const iso8601 = require('iso8601');
 const progress = require('progress-stream');
 
 
+const backup_name = 'flynn-backup-' + iso8601.fromDate(new Date()) + '.tar';
 const endpoint = new AWS.Endpoint(process.env.S3_ENDPOINT);
 const s3 = new AWS.S3({
     endpoint: endpoint,
@@ -19,9 +21,10 @@ const progress_stream = progress({time: 100 /* ms */}, function (progress) {
     console.log('down', progress);
 });
 
+
 got.stream('https://' + process.env.FLYNN_ENDPOINT + '/backup', {auth: ':' + process.env.FLYNN_TOKEN})
     .pipe(progress_stream)
-    .pipe(UploadStream(s3, {Bucket: process.env.S3_BUCKET_NAME, Key: 'backup.tar'}))
+    .pipe(UploadStream(s3, {Bucket: process.env.S3_BUCKET_NAME, Key: backup_name}))
     .on("error", function (err) {
         console.error(err);
     })
