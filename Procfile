77d619d732de0300cdbcfa9db0aed427638d7516
Ben Jeffrey <mail@benjeffrey.net>
2017-11-15 06:20:16 +0000

    Download backup from Flynn controller and upload to S3.

---
+++ Procfile
@@ -0,0 +1,1 @@
+backup: while true; do node backup.js; sleep 86400; done
---
+++ backup.js
@@ -0,0 +1,30 @@
+const AWS = require("aws-sdk");
+const UploadStream = require("s3-stream-upload");
+const got = require('got');
+const progress = require('progress-stream');
+
+
+const endpoint = new AWS.Endpoint(process.env.S3_ENDPOINT);
+const s3 = new AWS.S3({
+    endpoint: endpoint,
+    accessKeyId: process.env.S3_ACCESS_KEY_ID,
+    secretAccessKey: process.env.S3_ACCESS_KEY_SECRET,
+});
+
+// Flynn controller certificates are self-signed
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+
+const progress_stream = progress({time: 100 /* ms */}, function (progress) {
+    console.log('down', progress);
+});
+
+got.stream('https://' + process.env.FLYNN_ENDPOINT + '/backup', {auth: ':' + process.env.FLYNN_TOKEN})
+    .pipe(progress_stream)
+    .pipe(UploadStream(s3, {Bucket: process.env.S3_BUCKET_NAME, Key: 'backup.tar'}))
+    .on("error", function (err) {
+        console.error(err);
+    })
+    .on("finish", function () {
+        console.log("File uploaded!");
+    });
