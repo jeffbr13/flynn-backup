b804129144f28aec36beb27ad21406acdf525827
Ben Jeffrey <mail@benjeffrey.net>
2017-11-16 00:17:48 +0000

    Refactor and remove progress-stream dependency.
    Flynn controller /backup endpoint doesn't return
    a Content-Length header, so we don't know how big
    the backup file is before it's finished.

--- backup.js
+++ backup.js
@@ -2,10 +2,8 @@ const AWS = require("aws-sdk");
 const UploadStream = require("s3-stream-upload");
 const got = require('got');
 const iso8601 = require('iso8601');
-const progress = require('progress-stream');
 
 
-const backup_name = 'flynn-backup-' + iso8601.fromDate(new Date()) + '.tar';
 const endpoint = new AWS.Endpoint(process.env.S3_ENDPOINT);
 const s3 = new AWS.S3({
     endpoint: endpoint,
@@ -13,18 +11,12 @@ const s3 = new AWS.S3({
     secretAccessKey: process.env.S3_ACCESS_KEY_SECRET,
 });
 
-// Flynn controller certificates are self-signed
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";     // don't check Flynn controller self-signed certificates
+const url = 'https://' + process.env.FLYNN_ENDPOINT + '/backup';
+const backup_file_key = 'flynn-backup-' + iso8601.fromDate(new Date()) + '.tar';
 
-
-const progress_stream = progress({time: 100 /* ms */}, function (progress) {
-    console.log('down', progress);
-});
-
-
-got.stream('https://' + process.env.FLYNN_ENDPOINT + '/backup', {auth: ':' + process.env.FLYNN_TOKEN})
-    .pipe(progress_stream)
-    .pipe(UploadStream(s3, {Bucket: process.env.S3_BUCKET_NAME, Key: backup_name}))
+got.stream(url, {auth: ':' + process.env.FLYNN_TOKEN})
+    .pipe(UploadStream(s3, {Bucket: process.env.S3_BUCKET_NAME, Key: backup_file_key}))
     .on("error", function (err) {
         console.error(err);
     })
